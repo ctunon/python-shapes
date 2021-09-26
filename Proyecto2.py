a28527f46464ddf46fa2f80da21d02686511d3a3
Carlos Tuñón <ctunon@ayudinga.org>
2021-09-25 22:03:16 -0500

    Flood_Incorporation

--- Proyecto2.py
+++ Proyecto2.py
@@ -1,13 +1,37 @@
 # Carlos Tuñón 8-1066-1018
 # Sergio Rodríguez 8-980-2414
 
-from PIL import Image
+from PIL import Image, ImageTk
 from math import cos, sin, pi
-
+from queue import Queue
 import tkinter as tk
-img = Image.new("RGB", (300, 300))
+global imgtk
 
- 
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+
+
+
+window = tk.Tk()
+window.geometry("600x400")
+
+label1 = tk.Label(window, text="Hello", bg="red", fg="white" )
+label1.pack(fill=tk.X)
+
+canvas = tk.Canvas(window, width = 300, height = 300)
+canvas.pack(side= tk.BOTTOM)  
+  
+boton_octagono = tk.Button(window, text="Octagono", bg="black", fg="white", command = lambda: draw_octagon(150,150,75))
+boton_octagono.pack(side=tk.LEFT)
+
+boton_rombo = tk.Button(window, text="rombo",  bg="black", fg="white", command = lambda: draw_diamond(150,150,75))
+boton_rombo.pack(side=tk.LEFT)
+
+boton_rectangulo = tk.Button(window, text="rectangulo",  bg="black", fg="white", command = lambda: draw_square())
+boton_rectangulo.pack(side=tk.LEFT)
+
 
 def get_line(p0, p1):
     
@@ -48,12 +72,32 @@ def get_line(p0, p1):
         points.reverse()
     return points
 
+def dfs(img, x, y, new_color):
+    w = img.width
+    h = img.height
+    old_color = img.getpixel((x,y))
+    queue= Queue()
+    queue.put((x,y))
+    while not queue.empty():
+        x, y = queue.get()
+        if x < 0 or x >= w or y < 0 or y >= h or img.getpixel((x,y)) != old_color:
+            continue
+        else:   
+            img.putpixel((x, y),new_color)
+            queue.put((x+1, y))
+            queue.put((x-1, y))
+            queue.put((x, y+1))
+            queue.put((x, y-1))
+
 
 def draw_line(image, p0, p1):
     for pixel in get_line(p0, p1):
         image.putpixel(pixel, (255, 255, 255, 100))
     
 def draw_octagon(x,y,r):
+
+    img = Image.new("RGB", (300, 300))
+
     
     rad = (pi/180)*45
 
@@ -64,9 +108,19 @@ def draw_octagon(x,y,r):
         y1 = int(y + r * sin(rad*(i+1)))
 
         draw_line(img, (x0,y0), (x1,y1))
-    img.save(fp="images/canvas.jpg")
+
+    dfs(img,150,150,RED)
+    img.save(fp="images/octagon.jpg")
+
+    imgtk = ImageTk.PhotoImage(Image.open("images/octagon.jpg"))
+    canvas.create_image(20,20, anchor=tk.NW, image=imgtk) 
+    canvas.image = imgtk 
+
 
 def draw_diamond(x,y,r):
+
+    img = Image.new("RGB", (300, 300))
+
 
     rad = (pi/180)*90
 
@@ -77,10 +131,17 @@ def draw_diamond(x,y,r):
         y1 = int(y + r * sin(rad*(i+1)))
 
         draw_line(img, (x0,y0), (x1,y1))
-
-    img.save(fp="images/canvas.jpg")
+    
+    dfs(img,150,150,RED)
+    img.save(fp="images/diamond.jpg")
+    imgtk = ImageTk.PhotoImage(Image.open("images/diamond.jpg"))
+    canvas.create_image(20,20, anchor=tk.NW, image=imgtk) 
+    canvas.image = imgtk
 
 def draw_triangle(x,y,r):
+
+    img = Image.new("RGB", (300, 300))
+
     
     rad = (pi/180)*120
 
@@ -92,38 +153,27 @@ def draw_triangle(x,y,r):
 
         draw_line(img, (x0,y0), (x1,y1))
 
+    dfs(img,150,150,RED)
+    img.save(fp="images/triangle.jpg")
+    imgtk = ImageTk.PhotoImage(Image.open("images/octagon.jpg"))
+    canvas.create_image(20,20, anchor=tk.NW, image=imgtk) 
+    canvas.image = imgtk
     
 
-def draw_rectangle():
+def draw_square():
+
+    img = Image.new("RGB", (300, 300))
+
         
-    draw_line(img, (1000,500),(300,500))
-    draw_line(img, (300,500),(300,100))
-    draw_line(img, (300,100),(1000,100))
-    draw_line(img, (1000,100),(1000,500))
-
-
-
-
-
-img.save(fp="images/canvas.jpg")
-
-window = tk.Tk()
-window.geometry("600x400")
-
-label1 = tk.Label(window, text="Hello", bg="red", fg="white" )
-label1.pack(fill=tk.X)
-
-canvas = tk.Canvas(window, width = 300, height = 300)
-canvas.pack(side= tk.BOTTOM)  
-
-boton_octagono = tk.Button(window, text="Octagono", bg="black", fg="white", command = lambda: draw_octagon(150,150,75))
-boton_octagono.pack(side=tk.LEFT)
-
-boton_rombo = tk.Button(window, text="rombo",  bg="black", fg="white", command = lambda: draw_diamond(150,150,75))
-boton_rombo.pack(side=tk.LEFT)
-
-boton_rectangulo = tk.Button(window, text="rectangulo",  bg="black", fg="white", command = lambda: draw_rectangle())
-boton_rectangulo.pack(side=tk.LEFT)
+    draw_line(img, (225,225),(75,225))
+    draw_line(img, (75,225),(75,75))
+    draw_line(img, (75,75),(225,75))
+    draw_line(img, (225,75),(225,225))
+    dfs(img,150,150,RED)
+    img.save(fp="images/square.jpg")
+    imgtk = ImageTk.PhotoImage(Image.open("images/square.jpg"))
+    canvas.create_image(20,20, anchor=tk.NW, image=imgtk) 
+    canvas.image = imgtk
 
 
 window.mainloop()
